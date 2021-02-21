a59c4b1d12ac6f5def94c5fab5fa1e642bfc8688
ianfelix <ianfelix030@gmail.com>
2021-02-21 14:33:40 -0300

    feat: creat global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Layout } from './shared/components/Layout';
 import GlobalStyles from './shared/styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import dark from './shared/styles/themes/dark';
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
